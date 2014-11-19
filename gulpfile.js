6244ebdd8d02e8be01aa2ea251ba8c130d1d8c86
Brian Han <bthan@us.ibm.com>
2014-11-19 13:06:18 -0600

    Updated gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp      = require('gulp');
+var plumber   = require('gulp-plumber');
+var sass      = require('gulp-sass');
+var webserver = require('gulp-webserver');
+
+var sourcePaths = {
+  styles: ['scss/**/*.scss']
+};
+
+var distPaths = {
+  styles: 'public/css'
+};
+
+var server = {
+  host: 'localhost',
+  port: '3000'
+}
+
+// Compile scss files to css
+gulp.task('sass', function () {
+  gulp.src( sourcePaths.styles )
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(gulp.dest( distPaths.styles ));
+});
+
+// Run a local webserver 
+gulp.task('webserver', function() {
+  gulp.src('.')
+    .pipe(webserver({
+      host: server.host, 
+      port: server.port, 
+      livereload: true,
+      directoryListing: false
+    }));
+});
+
+// Automatically open browser to the server.host and port 
+gulp.task('openbrowser', function() {
+  opn( 'http://' + server.host + ':' + server.port );
+});
+
+gulp.task('watch', function() {
+  gulp.watch(sourcePaths.styles, ['sass']);
+});
+
+gulp.task('build', ['sass']);
+
+gulp.task('default', ['build', 'webserver', 'watch']);
